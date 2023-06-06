57166862521e6bfe5bc5beb06522c8c678514679
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-06-06 16:50:17 -0300

    EXERCÍCIO CONCLÚIDO

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("-=-Múltiplo-=-");
+
+Console.WriteLine("Por favor, digite um número: ");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Por favor, digite um segundo número:");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+    if (numero1 % numero2 == 0)
+        {
+            Console.WriteLine($"\n{numero1} é múltiplo de {numero2}");
+    
+        }
+    else
+        {
+            Console.WriteLine($"\n {numero1} não é múltiplo de {numero2}");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
